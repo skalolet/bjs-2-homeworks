f606890fdf76499483f5013eb0f36350bc55bd88
skalolet <skalolet@gmail.com>
2023-06-19 13:59:17 +0300

    Changed 6.exception-closure -- task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,52 @@
-﻿
+﻿function parseCount(value) {
+    const parsedValue = Number.parseFloat(value);
+    if (Number.isNaN(parsedValue)) {
+        throw new Error('Невалидное значение');
+    }
+    return parsedValue;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(side1, side2, side3) {
+        if (!this.isValidTriangle(side1, side2, side3)) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    isValidTriangle(side1, side2, side3) {
+        return side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1;
+    }
+
+    get perimeter() {
+        return this.side1 + this.side2 + this.side3;
+    }
+
+    get area() {
+        const s = this.perimeter / 2;
+        const area = Math.sqrt(s * (s - this.side1) * (s - this.side2) * (s - this.side3));
+        return area.toFixed(3);
+    }
+}
+
+function getTriangle(side1, side2, side3) {
+    try {
+        return new Triangle(side1, side2, side3);
+    } catch (error) {
+        return {
+            area: 'Ошибка! Треугольник не существует',
+            perimeter: 'Ошибка! Треугольник не существует',
+        };
+    }
+}
